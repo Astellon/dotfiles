8fcb0a7a8c961d6cf7b8cb6f448a105a3041b357
Astellon <astellon2525otastepper@gmail.com>
2019-02-27 16:58:03 +0900

    fix link folder

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 DOT_FILES=( .config .hyper.js .vimrc .xinitrc .xprofile .Xresources .zshenv .zshrc )
 for file in ${DOT_FILES[@]}
 do
-    ln -s $HOME/dotfiles/$file $HOME/$file
+    ln -s $HOME/dotfiles/$file $HOME/
 done
